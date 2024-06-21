c3ab26f3b3a054f5e2d6c0f9afb515c7a8707cbe
WillianGomes949 <williangomes949@gmail.com>
2024-06-21 11:26:29 -0300

    Criacao dos cards

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,16 @@
+import styles from './Card.module.css';
+import iconeFavoritar from './favoritar.png';
+
+export default function Card({ id, titulo, capa }) {
+  return (
+    <div className={styles.container}>
+      <img src={capa} alt={titulo} className={styles.capa} />
+      <h2>{titulo}</h2>
+      <img
+        src={iconeFavoritar}
+        alt="Favoritar filme"
+        className={styles.favoritar}
+      />
+    </div>
+  );
+}
--- src/pages/Inicio/index.jsx
+++ src/pages/Inicio/index.jsx
@@ -1,7 +1,10 @@
 import Banner from 'components/Banners';
 import Cabecalho from 'components/Cabecalho/Index';
+import Card from 'components/Card';
 import Rodape from 'components/Rodape';
 import Titulo from 'components/Titulo';
+import videos from 'json/db.json';
+import styles from './Inicio.module.css';
 
 function Inicio() {
   return (
@@ -11,6 +14,11 @@ function Inicio() {
       <Titulo>
         <h1>Um lugar para guardar videos e filmes!</h1>
       </Titulo>
+      <section className={styles.container}>
+        {videos.map((video) => {
+          return <Card {...video} key={video.id} />;
+        })}
+      </section>
       <Rodape />
     </>
   );
