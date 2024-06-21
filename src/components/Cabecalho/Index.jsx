ebbef1999e7095acc91a407f239071bf197ea05e
WillianGomes949 <williangomes949@gmail.com>
2024-06-21 09:28:47 -0300

    criacao de router

---
+++ src/components/Cabecalho/Index.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+import logo from './logo.png';
+import styles from './Cabecalho.module.css';
+import CabecalhoLink from 'components/CabecalhoLink';
+
+function Cabecalho() {
+  return (
+    <header className={styles.cabecalho}>
+      <Link to="./">
+        <img src={logo} alt="Logo Cinetag" />
+      </Link>
+      <nav>
+        <CabecalhoLink url="./">Home</CabecalhoLink>
+        <CabecalhoLink url="./Favoritos">Favoritos</CabecalhoLink>
+      </nav>
+    </header>
+  );
+}
+
+export default Cabecalho;
---
+++ src/pages/Inicio/index.jsx
@@ -0,0 +1,7 @@
+import Cabecalho from 'components/Cabecalho/Index';
+
+function Inicio() {
+  return <Cabecalho></Cabecalho>;
+}
+
+export default Inicio;
